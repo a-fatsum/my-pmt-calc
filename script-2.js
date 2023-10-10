872d671cb8eb8ab0db7675fafee4b98f44a04c9c
Mustafa Abdelati <mustafaabdelati@gmail.com>
2023-10-10 20:03:53 +1100

    added the functionality to create input fields and fates fields based on the number of months in the arrangement

--- script-2.js
+++ script-2.js
@@ -11,7 +11,10 @@ const fortnightlyWeeklyButton = document.querySelector(
 // ----
 fortnightlyWeeklyButton.addEventListener("click", () => {
   // hide monthly arrangement container
-  document.querySelector(".monthly-arrangement-details").style.display = "none";
+  document
+    .getElementById("month-wrapper")
+    .classList.replace("show-monthly", "monthly-arrangement-details");
+
   // show payment-details container
   document.querySelector(".payment-details").classList.replace("hide", "show");
   // show arrangement-deails-container
@@ -24,8 +27,10 @@ fortnightlyWeeklyButton.addEventListener("click", () => {
 });
 // ---
 monthlyButton.addEventListener("click", () => {
-  document.querySelector(".monthly-arrangement-details").style.display =
-    "block";
+  document
+    .getElementById("month-wrapper")
+    .classList.replace("monthly-arrangement-details", "show-monthly");
+
   //
   document.querySelector(".payment-details").classList.replace("show", "hide");
   //
@@ -142,38 +147,73 @@ const main_function = function () {
   // //
   // console.log(calculateMonthly());
   // ===================>>>>>>>>>>>>>>>
-  let month_input_1 = document.querySelector(".month-input-1");
-  let month_input_2 = document.querySelector(".month-input-2");
-  let month_input_3 = document.querySelector(".month-input-3");
-
   //
-
-  month_input_1.value = calculateMonthly().toFixed(2);
-  month_input_2.value = calculateMonthly().toFixed(2);
-  month_input_3.value = calculateMonthly().toFixed(2);
-
-  month_input_1.addEventListener("keyup", () => {
-    const remaining_balance =
-      Number(total_monthly_arrangement) - Number(month_input_1.value);
-    month_input_2.value = Number(remaining_balance) / 2;
-    month_input_3.value = Number(remaining_balance) / 2;
-  });
-  month_input_2.addEventListener("keyup", () => {
-    const remaining_balance =
-      Number(total_monthly_arrangement) - Number(month_input_2.value);
-    month_input_1.value = Number(remaining_balance) / 2;
-    month_input_3.value = Number(remaining_balance) / 2;
-  });
-  month_input_3.addEventListener("keyup", () => {
-    const remaining_balance =
-      Number(total_monthly_arrangement) - Number(month_input_3.value);
-    month_input_1.value = Number(remaining_balance) / 2;
-    month_input_2.value = Number(remaining_balance) / 2;
-  });
-
-  //
-
   // ===================>>>>>>>>>>>>>>>
+  // ===================>>>>>>>>>>>>>>>
+
+  // for (let index = 0; index < numOfMonths; index++) {
+  //   console.log(Number(index) + 1);
+  //   let month_input = `month_input_${Number(index) + 1}`;
+  //   console.log(month_input);
+  // }
+
+  // ---------->>>>
+  // ---------->>>>
+
+  // ---------->>>> Creat input fields for months amounts
+  for (let index = 0; index < numOfMonths; index++) {
+    var month_input_ = document.createElement("INPUT");
+    console.log(month_input_);
+    const months_input_container = document.getElementById(
+      "months-input-container"
+    );
+    month_input_.classList.add("months");
+    months_input_container.append(month_input_);
+  }
+
+  // ---------->>>> Creat input fields for dates
+
+  for (let index = 0; index < numOfMonths; index++) {
+    var month_date_field = document.createElement("DIV");
+    console.log(month_date_field);
+    const months_dates_container = document.getElementById(
+      "months-dates-containter"
+    );
+    month_date_field.classList.add("month");
+    months_dates_container.append(month_date_field);
+  }
+
+  // ---------->>>>
+  // ---------->>>>
+
+  // month_input_2.value = calculateMonthly().toFixed(2);
+  // month_input_3.value = calculateMonthly().toFixed(2);
+
+  // month_input_1.addEventListener("keyup", () => {
+  //   const remaining_balance =
+  //     Number(total_monthly_arrangement) - Number(month_input_1.value);
+  //   month_input_2.value = Number(remaining_balance) / 2;
+  //   month_input_3.value = Number(remaining_balance) / 2;
+  // });
+  // month_input_2.addEventListener("keyup", () => {
+  //   const remaining_balance =
+  //     Number(total_monthly_arrangement) - Number(month_input_2.value);
+  //   month_input_1.value = Number(remaining_balance) / 2;
+  //   month_input_3.value = Number(remaining_balance) / 2;
+  // });
+  // month_input_3.addEventListener("keyup", () => {
+  //   const remaining_balance =
+  //     Number(total_monthly_arrangement) - Number(month_input_3.value);
+  //   month_input_1.value = Number(remaining_balance) / 2;
+  //   month_input_2.value = Number(remaining_balance) / 2;
+  // });
+
+  //
+  // ===================>>>>>>>>>>>>>>>
+  // ===================>>>>>>>>>>>>>>>
+  // ===================>>>>>>>>>>>>>>>
+
+  // ===================>>>>>>>>>>>>>>> Add dates into dates fields
   const list_of_dates_in_arrg = [];
   list_of_dates_in_arrg.push(instalementDueFullDate(billingDay));
   let monthlyDates = instalementDueFullDate(billingDay);
@@ -184,11 +224,15 @@ const main_function = function () {
     list_of_dates_in_arrg.push(monthlyDates);
   }
   console.log(list_of_dates_in_arrg);
-  let x = document.getElementsByClassName("month");
-  for (let i = 0, len = x.length; i <= len; i++) {
-    x[i].innerHTML = list_of_dates_in_arrg[i].toDateString();
+
+  let list_of_elements = document.getElementsByClassName("month");
+  for (let i = 0, len = list_of_elements.length; i <= len; i++) {
+    list_of_elements[i].innerHTML = list_of_dates_in_arrg[i].toDateString();
   }
+  // ===================>>>>>>>>>>>>>>>
   //
+
+  // ===================>>>>>>>>>>>>>>> Add monthly amounts to monthly inputs
 };
 
 // =================================
@@ -202,7 +246,7 @@ const lateFees = 25;
 const numOfMonths = document.querySelector(".num-of-months");
 //
 
-overdue.addEventListener("keyup", main_function);
-monthlyInstalment.addEventListener("keyup", main_function);
+overdue.addEventListener("input", main_function);
+monthlyInstalment.addEventListener("input", main_function);
 billingDay.addEventListener("click", main_function);
-numOfMonths.addEventListener("keyup", main_function);
+numOfMonths.addEventListener("input", main_function);
